99081f3bd9c41a5d63ebed559f7a2f64ee45779c
Asmaa Elsayed <50335819+asmaaelsayed77@users.noreply.github.com>
2019-06-30 16:40:41 -0400

    Add files

---
+++ js/index.js
@@ -0,0 +1,60 @@
+let pattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+// event-target object
+let btn = document.getElementById("submit-btn");
+let email = document.getElementById("email");
+let fullName = document.getElementById("full-name");
+let message = document.getElementById("message");
+
+// Clear the data from HTML form
+let testForm = document.querySelector("form");
+
+// event-handler
+function validateForm() {
+    
+    console.clear();
+    
+    // save data and errors in these initially empty "buckets":
+    let errors = [];
+    let data = {};
+    
+    // VALIDATING FULL NAME
+    if (fullName.value === ""){
+        errors.push("Please enter your full name.");
+    } else {
+        data.fullname = fullName.value;
+    } 
+    
+    // VALIDATING EMAIL
+    if (email.value === "") {
+        errors.push("Please enter your email!");
+    } else {
+        if (pattern.test(email.value)) {
+            data.email = email.value;
+        } else {
+            errors.push("your email not correct");
+        }
+    }
+	
+	//VALIDATING MESSAGE
+	if (message.value === "") {
+		errors.push("Please write your message");
+	}else {
+		data.message = message.value;
+	}
+    
+    
+    
+    
+    // CREATE FEEDBACK
+    if (errors.length === 0) {
+        // clear your form here:
+        testForm.reset();
+        console.log(data);
+    } else {
+        console.log(errors);
+    } 
+}
+
+// registering event
+btn.addEventListener("click", validateForm);
